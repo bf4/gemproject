a29d21210db37fb83bb235bb7c55f9a885f51729
Benjamin Fleischer <github@benjaminfleischer.com>
2013-07-11 15:38:23 -0500

    Move up the current public interface

--- run.rb
+++ run.rb
@@ -15,18 +15,66 @@ class GemDownloads
     update(store)
   end
   # gems = Gems.gems
+  # ["name", "downloads", "version", "version_downloads", "platform", "authors", "info", "project_uri", "gem_uri", "homepage_uri", "wiki_uri", "documentation_uri", "mailing_list_uri", "source_code_uri", "bug_tracker_uri", "dependencies"]
   # Gems.dependencies ['metric_fu']
   # TypeError: incompatible marshal file format (can't be read)
   # Gems.total_downloads 'metric_fu'
   #
-  # ["name", "downloads", "version", "version_downloads", "platform", "authors", "info", "project_uri", "gem_uri", "homepage_uri", "wiki_uri", "documentation_uri", "mailing_list_uri", "source_code_uri", "bug_tracker_uri", "dependencies"]
+  #see Gems.search, yank, unyank, owners
+  # .total_downlods, name, version
+  def new_store(name = 'zgem_metrics.yml')
+    @store = YAML::Store.new(name)
+  end
+
+  def store
+    @store ||= new_store
+  end
+
+  def latest_downloads(store)
+    latest.each do |gem_name|
+      @logger.info "Getting download info for #{gem_name}"
+      store.transaction do
+        store[gem_name] = downloads(gem_name)
+      end
+    end
+  end
+
+  def update(store)
+    timestamp = datetime
+    gems.each do |gem|
+      puts
+      gem_name = gem['name']
+      print gem_name
+      dependencies = gem['dependencies']
+      info = gem['info']
+      url = gem['homepage_uri'] || gem['source_code_uri'] || gem['project_uri']
+      store.transaction do
+        store[gem_name] ||= {}
+        store[gem_name]['info'] = info
+        store[gem_name]['url'] = url
+        gem_releases(gem_name).each do |gem_release|
+          version =  gem_release['number']
+          print " #{version}"
+          count =       gem_release['downloads_count']
+          build_date =  gem_release['built_at']
+          @logger.info "Updating downloads for #{gem_name} version #{version} built at #{build_date} to #{count} on #{timestamp}"
+          store[gem_name][version] ||= {}
+          store[gem_name][version]['build_date'] = build_date
+          store[gem_name][version]['downloads'] ||= {}
+          store[gem_name][version]['downloads'][count] = timestamp
+        end
+        store[gem_name]['dependencies'] = dependencies
+      end
+    end
+  end
+
+  private
+
   def my_gems
     @gems ||= Gems.gems # implicit is my credentials
   end
-  #see Gems.search, yank, unyank, owners
-  # .total_downlods, name, version
-  # Gems.downloads name, version, from, to # number of downloads by day
 
+  # Gems.downloads name, version, from, to # number of downloads by day
   def downloads(gem_name, from=nil, to=Date.today)
     @result = {}
     puts
@@ -66,14 +114,6 @@ class GemDownloads
   def updated_gems
     Gems.just_updated
   end
-  def latest_downloads(store)
-    latest.each do |gem_name|
-      @logger.info "Getting download info for #{gem_name}"
-      store.transaction do
-        store[gem_name] = downloads(gem_name)
-      end
-    end
-  end
   def gems
     my_gems.concat(extra_gems).
       reject! {|gem| rejected_gems.include?(gem['name']) }
@@ -99,45 +139,10 @@ class GemDownloads
     @rejected_gems ||= %w(bf4-metrical bf4-metric_fu bf4-yui-rails bf4-browsercms bf4-bcms_news)
   end
 
-  def new_store(name = 'zgem_metrics.yml')
-    @store = YAML::Store.new(name)
-  end
-
-  def store
-    @store ||= new_store
-  end
 
   def datetime
     p Time.now.utc.xmlschema
   end
 
-  def update(store)
-    timestamp = datetime
-    gems.each do |gem|
-      puts
-      gem_name = gem['name']
-      print gem_name
-      dependencies = gem['dependencies']
-      info = gem['info']
-      url = gem['homepage_uri'] || gem['source_code_uri'] || gem['project_uri']
-      store.transaction do
-        store[gem_name] ||= {}
-        store[gem_name]['info'] = info
-        store[gem_name]['url'] = url
-        gem_releases(gem_name).each do |gem_release|
-          version =  gem_release['number']
-          print " #{version}"
-          count =       gem_release['downloads_count']
-          build_date =  gem_release['built_at']
-          @logger.info "Updating downloads for #{gem_name} version #{version} built at #{build_date} to #{count} on #{timestamp}"
-          store[gem_name][version] ||= {}
-          store[gem_name][version]['build_date'] = build_date
-          store[gem_name][version]['downloads'] ||= {}
-          store[gem_name][version]['downloads'][count] = timestamp
-        end
-        store[gem_name]['dependencies'] = dependencies
-      end
-    end
-  end
 end
 GemDownloads.new
