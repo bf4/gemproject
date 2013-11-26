004551aadafc4644938c3f3736daa4c7312e8fbc
Benjamin Fleischer <github@benjaminfleischer.com>
2013-11-26 07:58:50 -0600

    Update downloads

--- loop.rb
+++ loop.rb
@@ -1,2 +1,10 @@
 @minutes = 60*2
-loop { p "Beginning run #{Time.now}"; system('./update_stats.sh'); p "Ending Running #{Time.now}. Sleeping #{@minutes} minutes"; sleep 60*@minutes; p "Done sleeping at #{Time.now}" }
+loop {
+  mem_usage =  `ps aux | grep ruby #{$0} | awk '{sum +=$4}; END {print sum}'`
+  p "script memory usage #{mem_usage}"
+  p "Beginning run #{Time.now}"
+  system('./update_stats.sh')
+  p "Ending Running #{Time.now}. Sleeping #{@minutes} minutes"
+  sleep 60*@minutes
+  p "Done sleeping at #{Time.now}"
+}
